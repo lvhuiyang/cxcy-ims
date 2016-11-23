108280748761f5348702b6c07faa6da5a44aaf41
lvhuiyang <i@lvhuiyang.cn>
2016-11-23 21:39:09 +0800

    用户更密码

--- app/main/views/user.py
+++ app/main/views/user.py
@@ -1,9 +1,12 @@
 import os
 from flask import render_template, request, flash, redirect, url_for
+from flask_login import login_required
 from werkzeug.utils import secure_filename
+from app import db
 from app.main import main
+from app.decorators import user_required
 from app.create_random_str import get_random_str
-from app.models import Project, Competition
+from app.models import Project, Competition, User
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
@@ -14,13 +17,63 @@ def allowed_file(filename):
 
 
 @main.route('/user')
+@login_required
+@user_required
 def user_index():
+    """
+    普通用户登录后的首页
+    :return:
+    """
     return render_template('user.html')
 
 
 @main.route('/user/profile')
+@login_required
+@user_required
 def user_profile():
+    """
+    用户状态查看与密码修改
+    :return:
+    """
     return render_template("user/user_account.html")
+
+
+@main.route('/user/change_password', methods=['GET', 'POST'])
+@login_required
+@user_required
+def user_change_password():
+    """
+    用户密码修改的请求
+    :return:
+    """
+    if request.method == 'GET':
+        return redirect(url_for('main.user_profile'))
+    else:
+        username = request.form['username']
+        old_password = request.form['old_password']
+        new_password = request.form['new_password']
+        new_password_confirm = request.form['new_password_confirm']
+        print(username + '   ' + old_password + '  ' + new_password_confirm)
+        try:
+            this_user = User.query.filter_by(username=username).first()
+            if (this_user.verify_password(old_password)):
+                # 原密码正确
+                if new_password == new_password_confirm:
+                    this_user.password = new_password
+                    flash("密码修改成功")
+                    return redirect(url_for('main.user_profile'))
+                else:
+                    flash("XXXXX")
+                    return redirect(url_for('main.user_profile'))
+            else:
+                # 原密码不正确
+                flash("原密码不正确请您重新操作")
+                return redirect(url_for('main.user_profile'))
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            flash("未知错误")
+        return redirect(url_for('main.user_profile'))
 
 
 @main.route('/user/submit_history')
@@ -30,6 +83,10 @@ def user_submit_history():
 
 @main.route('/user/competition', methods=['GET', 'POST'])
 def user_competition():
+    """
+    进行比赛项目的提交
+    :return:
+    """
     if request.method == 'GET':
         project = request.args.get('project_id')
         if project is not None:
