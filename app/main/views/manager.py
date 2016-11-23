2bdfa0c9fcd3fe0e1d8ec5d50938c3c6e3c13eda
lvhuiyang <i@lvhuiyang.cn>
2016-11-23 17:00:44 +0800

    添加新项目名

--- app/main/views/manager.py
+++ app/main/views/manager.py
@@ -3,8 +3,8 @@ from flask import render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from app import db
 from app.main import main
-from app.models import User
-from app.decorators import manager_required, user_required
+from app.models import User, Project
+from app.decorators import manager_required
 
 
 @main.route('/manager')
@@ -53,3 +53,35 @@ def manager_add_user():
 def manager_user_list():
     users = User.query.all()
     return render_template('manager/user_list.html', users=users)
+
+
+@main.route('/manager/add_new_project', methods=['GET', 'POST'])
+@login_required
+@manager_required
+def manager_add_new_project():
+    if request.method == 'GET':
+        return render_template('manager/add_new_project.html')
+    else:
+        try:
+            project_name = request.form['project_name']
+            project_obj = Project(
+                name=project_name,
+                created_date=datetime.today(),
+                created_people=current_user.username
+            )
+            db.session.add(project_obj)
+            db.session.commit()
+            flash("新添加项目：" + project_obj.name + "添加成功")
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            flash("未知错误")
+        return redirect(url_for('main.manager_add_new_project'))
+
+
+@main.route('/manager/project_list')
+@login_required
+@manager_required
+def manager_project_list():
+    projects = Project.query.all()
+    return render_template('manager/project_list.html', projects=projects)
